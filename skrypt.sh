f2257296c3fb1cfd2704d1a123cb123fb9313d7d
KarolOG <k.ogorzalek1@gmail.com>
2023-06-29 22:59:44 +0200

    dodanie skryptu z dodaną obsługą argumentu --help - wyświetlanie wszystkich dostępnych opcji

--- skrypt.sh
+++ skrypt.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 if [[ "$1" == "--date" ]]; then
-  echo "Dzisiejsza data: $(date)"
-elif [[ "$1" == "--logs" ]]; then
-  for ((i=1; i<=100; i++))
-  do
-    filename="log${i}.txt"
-    echo "Nazwa pliku: $filename" >> "$filename"
-    echo "Nazwa skryptu: skrypt.sh" >> "$filename"
-    echo "Data: $(date)" >> "$filename"
-  done
+	date
+fi
+
+if [[ "$1" == "--logs" ]]; then
+  	if [ -n "$2"]; then
+    		num_files=$2
+	else
+		num_files=100
+	fi
+	
+	for ((i=1; i<=num_files; i++)); do
+		filename="log${i}.txt"
+      		echo "Nazwa pliku: $filename" >> "$filename"
+      		echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+      		echo "Data: $(date)" >> "$filename"
+	done
+fi
+
+if [[ "$1" == "--help" ]]; then
+  	echo "Dostępne opcje:"
+  	echo "--date: Wyświetla dzisiejszą datę."
+  	echo "--logs <liczba_plików>: Tworzy określoną liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę."
+else
+  	echo "Nieznana opcja. Użyj 'skrypt.sh --help' aby wyświetlić dostępne opcje."
 fi
